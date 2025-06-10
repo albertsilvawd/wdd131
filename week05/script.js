132b28e2fae2183b0598169fc7218e5166b64f33
albertsilvawd <sil24011@byui.edu>
2025-06-10 17:35:59 -0300

    w05-03

---
+++ week05/script.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+// Load or create the chapters array from localStorage
+let chaptersArray = getChapterList() || [];
+
+// Render all chapters saved on load
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1).trim();
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
